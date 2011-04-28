c7d0159be1e50257aea1bd38ec08b4c0a51044f8
AVergos <vergosgeorge@hotmail.com>
2011-04-28 14:57:26 +0300

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    logo = image_tag("rails.png", :alt => "Blog App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Blog App"
